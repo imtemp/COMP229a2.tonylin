ac502f4a582afdcfb73e16624f1b5b38172ba039
Tony Lin <t.lin0919@gmail.com>
2020-10-07 21:50:21 -0400

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,15 @@
+(function(){
+
+    function Start()
+    {
+        console.log("App started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
+
+function messageSent(){
+    var sent = "Your message has been sent.";
+    alert (sent);
+}
